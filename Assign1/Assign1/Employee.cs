b5466553970d49b981f2ab3334d73ea2bffefeff
F249 Master <f249master@champlaincollege.qc.ca>
2021-10-01 13:53:41 -0400

    Init

---
+++ Assign1/Assign1/Employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assign1
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public bool Hockey { get; set; }
+
+        public bool Basketball { get; set; }
+
+        public bool None { get; set; }
+
+        public bool Day { get; set; }
+
+        public bool Evening { get; set; }
+
+        public string IsValid()
+        {
+            if (Name == null || Name.Length <= 0) return "Name not set";
+
+            if (Salary < 0) return "Salary cannot be negative";
+
+            if (Salary > 10000) return "Salary too high";
+
+            if (!(Hockey || Basketball || None)) return "Must choose a sport";
+
+            if (!Day && !Evening) return "Time must be set";
+
+            return null;
+        }
+
+    }
+}
---
+++ Assign1/Assign1/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Assign1
+{
+    public partial class MainPage : ContentPage
+    {
+        public Employee employee { get; set; }
+        public MainPage()
+        {
+            employee = new Employee();
+            InitializeComponent();
+            BindingContext = this;
+        }
+
+        private bool IsValid()
+        {
+            if (!(dayTime.IsChecked ^ evening.IsChecked)) return false;
+
+
+
+            return true;
+        }
+
+        async private void AddClicked(object sender, EventArgs e)
+        {
+            var errorMessage = employee.IsValid();
+
+            if(errorMessage != null)
+            {
+                await DisplayAlert("Alert", errorMessage, "OK");
+                return;
+            }
+
+            Console.WriteLine(employee.Salary);
+        }
+    }
+}
